54efff53a67c80268ff813b0350db0fcb0781f63
shimrink <shimrin.kirill@yandex.ru>
2022-06-05 10:07:38 +0300

    adaptiv for tablet

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,55 +2,23 @@ $(document).ready(function () {
     svg4everybody({});
 });
 
-
+$('.faq__answer').slideUp(500);
 // show and hide faq-block
-$('#faq__name_name1').click(function(){
-    $('#faq__answer_answer1').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer1').css('transition', 'height 0.3s');
-    $('#faq__svg_svg1').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg1').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name2').click(function(){
-    $('#faq__answer_answer2').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer2').css('transition', 'height 0.3s');
-    $('#faq__svg_svg2').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg2').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name3').click(function(){
-    $('#faq__answer_answer3').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer3').css('transition', 'height 0.3s');
-    $('#faq__svg_svg3').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg3').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name4').click(function(){
-    $('#faq__answer_answer4').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer4').css('transition', 'height 0.3s');
-    $('#faq__svg_svg4').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg4').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name5').click(function(){
-    $('#faq__answer_answer5').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer5').css('transition', 'height 0.3s');
-    $('#faq__svg_svg5').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg5').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name6').click(function(){
-    $('#faq__answer_answer6').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer6').css('transition', 'height 0.3s');
-    $('#faq__svg_svg6').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg6').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name7').click(function(){
-    $('#faq__answer_answer7').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer7').css('transition', 'height 0.3s');
-    $('#faq__svg_svg7').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg7').css('transition', 'transform 0.3s');
-});
-$('#faq__name_name8').click(function(){
-    $('#faq__answer_answer8').toggleClass('faq__answer_closed faq__answer_opened');
-    $('#faq__answer_answer8').css('transition', 'height 0.3s');
-    $('#faq__svg_svg8').toggleClass('faq__svg_closed faq__svg_opened');
-    $('#faq__svg_svg8').css('transition', 'transform 0.3s');
+$('.faq__name').click(function(){
+    var id=$(this).attr('id');
+    $('.faq__answer').slideUp(500);
+    if(!$(this).hasClass('faq__name_active')){
+        $('.faq__name').removeClass('faq__name_active');
+        $(this).addClass('faq__name_active');
+        $('#faqAnswer_'+id).slideDown(500);
+        $('.faq__svg').removeClass('faq__svg_opened');
+        $('#faqSvg_'+id).addClass('faq__svg_opened');
+        $('#faqSvg_'+id).css('transition', 'transform 0.3s');
+    } else {
+        $('.faq__name').removeClass('faq__name_active');
+        $('#faqSvg_'+id).removeClass('faq__svg_opened');
+        $('#faqSvg_'+id).css('transition', 'transform 0.3s');
+    }
 });
 
 //header and toTopButton hide
@@ -81,6 +49,10 @@ $('#burger').click(function(){
 });
 
 $('#burger-menu__close').click(function(){
+    $('.burger-menu').toggleClass('burger-menu_closed burger-menu_opened');
+});
+
+$('.burger-menu__contacts').click(function(){
     $('.burger-menu').toggleClass('burger-menu_closed burger-menu_opened');
 });
 
