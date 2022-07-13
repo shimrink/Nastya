4a666d777b9bfa9c86556685f240fdb2601413c0
shimrink <shimrin.kirill@yandex.ru>
2022-07-13 20:11:57 +0300

    tabs

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -114,6 +114,64 @@ window.addEventListener('dragstart',onDragStart);
 window.addEventListener('blur',onMouseUp);
 $('.carousel a').click(onMouseClick);
 
+//offer tabs
+$('#31').hide();
+$('.identity').hide();
+$('.more-services').hide();
+$('.offer__button').click(function() {
+    var num=$(this).attr('id');
+    if(!$(this).hasClass('offer__button_active')) {
+        $('.offer__button').removeClass('offer__button_active');
+        $(this).addClass('offer__button_active');
+        if(num == 31) {
+            $('#31').hide();
+            $('#321').show();
+            $('#33').show();
+            $('.look-wrap').show();
+            $('.web-design').show();
+            $('.identity').hide();
+            $('.more-services').hide();
+            $('.services__title').html('УСЛУГИ.<br>ВЕБ-ДИЗАЙН');
+        } else if(num == 321 || num == 322) {
+            $('#321').hide();
+            $('#322').hide();
+            $('#31').show();
+            $('#33').show();
+            $('.look-wrap').hide();
+            $('.web-design').hide();
+            $('.identity').show();
+            $('.more-services').hide();
+            $('.services__title').html('УСЛУГИ.<br>АЙДЕНТИКА');
+        } else if(num == 33) {
+            $('#31').show();
+            $('#322').show();
+            $('#33').hide();
+            $('.look-wrap').hide();
+            $('.web-design').hide();
+            $('.identity').hide();
+            $('.more-services').show();
+            $('.services__title').html('УСЛУГИ.<br>ПРОЧИЕ УСЛУГИ');
+        }
+    }
+});
+
+$('#41').addClass('look__button_active');
+$('.web-design__site').hide();
+$('.look__button').click(function() {
+    var num=$(this).attr('id');
+    if(!$(this).hasClass('look__button_active')) {
+        $('.look__button').removeClass('look__button_active');
+        $(this).addClass('look__button_active');
+        if(num == 41) {
+            $('.web-design__landing').show();
+            $('.web-design__site').hide();
+        } else if(num == 42) {
+            $('.web-design__site').show();
+            $('.web-design__landing').hide();
+        }
+    }
+});
+
 //animate text
 const animItems = document.querySelectorAll('._anim-items');
 
