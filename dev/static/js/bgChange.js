84c244e97efdebf8d82c64d86f1694b434a43941
shimrink <shimrin.kirill@yandex.ru>
2022-07-22 11:27:43 +0300

    webp

--- dev/static/js/bgChange.js
+++ dev/static/js/bgChange.js
@@ -2,8 +2,7 @@ $(document).scroll(function() {
     if($('body').hasClass('services-page') && ($(window).scrollTop() > $('#white-block_2').offset().top - 300 && $(window).scrollTop() < $('#white-block_2').offset().top + $('#white-block_2').innerHeight() - 300
     || $(window).scrollTop() > $('#white-block_3').offset().top - 300 && $(window).scrollTop() < $('#white-block_3').offset().top + $('#white-block_3').innerHeight() - 300
     || $(window).scrollTop() > $('#white-block_4').offset().top - 300 && $(window).scrollTop() < $('#white-block_4').offset().top + $('#white-block_4').innerHeight() - 300
-    || $(window).scrollTop() > $('#white-block_5').offset().top - 300 && $(window).scrollTop() < $('#white-block_5').offset().top + $('#white-block_5').innerHeight() - 300
-    || $(window).scrollTop() > $('#white-block_6').offset().top - 300 && $(window).scrollTop() < $('#white-block_6').offset().top + $('#white-block_6').innerHeight() - 300)
+    || $(window).scrollTop() > $('#white-block_5').offset().top - 300 && $(window).scrollTop() < $('#white-block_5').offset().top + $('#white-block_5').innerHeight() - 300)
     || $('body').hasClass('index-page') && ($(window).scrollTop() > $('#white-block_1').offset().top - 300 && $(window).scrollTop() < $('#white-block_1').offset().top + $('#white-block_1').innerHeight() - 300)) {
         $('.wrapper').addClass('wrapper__white');
         $('.card').addClass('card__white');
